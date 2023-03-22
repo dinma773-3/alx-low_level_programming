cb9c729decb577cf5a310243fb89757ad03eff03
root <godsfavourchukwudi21@gmail.com>
2023-03-21 23:00:42 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
+#include "main_h"
+/**
+ *main - print _putchar
+ *is written by Favour
+ *Return: 0
+ */
 
-int main() {
-  putchar('_');
-  putchar('p');
-  putchar('u');
-  putchar('t');
-  putchar('c');
-  putchar('h');
-  putchar('a');
-  putchar('r');
-  putchar('\n');
-  return 0;
+
+int main (void) {
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
 }
