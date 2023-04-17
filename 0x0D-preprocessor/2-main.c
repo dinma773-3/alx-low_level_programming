e8eea2a53f1489367515942bae7dd36e3a8e6366
root <godsfavourchukwudi21@gmail.com>
2023-04-17 09:20:48 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-#define FILE "__FILe_")
-
 /**
  * main - prints the name of the file it was compiled from
  *
  * Return: 0 (success)
  */
 int main(void) {
-  printf("%s\n", FILE);
+  printf("%s\n", __FILE__);
   return (0);
 }
